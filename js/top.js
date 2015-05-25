51991cfa7c554e1507444b714df67f99e55d3c99
candice <candice2cc@gmail.com>
2015-05-25 21:14:40 +0800

    format

--- js/top.js
+++ js/top.js
@@ -1,18 +1,18 @@
-+function($){
++function ($) {
     var menuHead = '.menu';
-    var Top = function(el){
-        $(el).on('click',menuHead,this.show);
+    var Top = function (el) {
+        $(el).on('click', menuHead, this.show);
     };
     Top.VERSION = '1.0.0';
     Top.TRANSITION_DURATION = 150;
 
-    Top.prototype.show = function(e){
+    Top.prototype.show = function (e) {
         var $this = $(this);
         $this.parents("ul").children("li").removeClass("active");
         $this.toggleClass("active");
         e.stopPropagation();
     };
-    Top.prototype.close = function(e){
+    Top.prototype.close = function (e) {
         $(menuHead).removeClass("active");
 
     };
@@ -22,12 +22,13 @@
     function Plugin(option) {
         return this.each(function () {
             var $this = $(this)
-            var data  = $this.data('bs.top')
+            var data = $this.data('bs.top')
 
             if (!data) $this.data('bs.top', (data = new Top(this)))
             if (typeof option == 'string') data[option].call($this)
         })
     }
+
     var old = $.fn.top;
     $.fn.top = Plugin;
     $.fn.top.Constructor = Top;
@@ -43,6 +44,6 @@
     // ==============
 
     $(document).on('click.bs.top.data-api', menuHead, Top.prototype.show);
-    $(document).on('click.bs.top.data-api',Top.prototype.close);
+    $(document).on('click.bs.top.data-api', Top.prototype.close);
 
 }(jQuery);
