93a665d30d35ad9425b7eb831120d106f121de3a
candice <candice2cc@gmail.com>
2015-05-28 10:24:59 +0800

    footer微信二维码替换

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     del = require('del'),
     less = require('gulp-less'),
-    path = require('path');
+    path = require('path'),
+    replace = require('gulp-replace');
 
 var LessPluginCleanCSS = require('less-plugin-clean-css'),
     LessPluginAutoPrefix = require('less-plugin-autoprefix'),
@@ -29,6 +30,7 @@ gulp.task('less', function () {
             paths: [ path.join(__dirname, 'less', 'includes') ],
             plugins: [autoprefix, cleancss]
         }))
+        .pipe(replace('background:#000;background-color:rgba(0,0,0,.75);','background: #000\\9;background-color:rgba(0,0,0,.75);'))
         .pipe(gulp.dest('./dist/css'))
         .pipe(notify({message: 'Less task complete'}));
 });
