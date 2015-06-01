e2d6b17a71eb19d798bb0b5f2f14244d0b0b4bc0
Kuangji <kuangji.yu@gmail.com>
2015-06-01 14:23:35 +0800

    commit temp

---
+++ js/search-bar.js
@@ -0,0 +1,15 @@
+/**
+ * @file searchbar.js
+ * @author yukuangji<yukuangji@bignox.com>
+ * @date 20150515
+ * @description search box with suggestion list
+ */
+
+'use strict';
+(function($){
+    function SearchBar(){
+
+    }
+    SearchBar.prototype = {};
+    $.fn.SearchBar = SearchBar;
+})(jQuery);
---
+++ js/search.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yukuangji on 15/5/25.
+ * @file search.js
+ * @author yukuangji<yukuangji@bignox.com>
+ * @date 20150515
+ * @description search box with suggestion list
+ *
+ */
+
+'use strict';
+(function(){
+
+})();
