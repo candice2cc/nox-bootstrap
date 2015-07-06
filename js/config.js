30de0d6e6c0569c1f699b33fd45c9dd1058374f1
candice <candice2cc@gmail.com>
2015-07-06 20:05:38 +0800

    消息中心使用线上地址

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 
 CONFIG = {};
 CONFIG.host = {
-    passport:"sj.bignox.com:9443",
+    passport:"passport.bignox.com",
 };
 CONFIG.url = {
   getMessage:"https://" + CONFIG.host.passport + "/usercenter/rest/thirdparty/message"
